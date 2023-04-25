8429b223de38bcf68dc775b9903a2780d4bb1e91
Liam Keegan <liam@keegan.ch>
2023-04-25 16:51:04 +0200

    cache files in serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,14 @@
+const urlsToCache = ["/app", "css/layout.css", "imgs/header.jpg"];
+
 self.addEventListener("install", (event) => {
   console.log("Service worker installed");
+  event.waitUntil(
+    caches.open("pwa-assets").then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
 });
+
 self.addEventListener("activate", (event) => {
   console.log("Service worker activated");
 });
