4b23fda64baed348ab93f3ad5fd1fa06277b92c1
Liam Keegan <liam@keegan.ch>
2023-04-26 09:45:44 +0200

    use cache if request fails for app

--- serviceworker.js
+++ serviceworker.js
@@ -2,8 +2,10 @@ const urlsToCache = ["/app", "css/layout.css", "imgs/header.jpg"];
 
 self.addEventListener("install", (event) => {
   console.log("Service worker installed");
+  // update cache of app page to serve offline
   event.waitUntil(
     caches.open("pwa-assets").then((cache) => {
+      console.log("Caching assets");
       return cache.addAll(urlsToCache);
     }),
   );
@@ -11,4 +13,14 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   console.log("Service worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  // if request fails, try to load from cache
+  event.respondWith(
+    fetch(event.request).catch((error) => {
+      console.log("Server offline - using cache");
+      return caches.match(event.request);
+    }),
+  );
 });
